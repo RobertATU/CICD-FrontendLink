3c3641d664cd43e169c88b43b59f84dba09373f9
Robert <g00395612@atu.ie>
2023-12-23 14:43:00 +0000

    Init commit

---
+++ src/main/java/ie/atu/cicdfrontend/CicdFrontendApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicdfrontend/CicdFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
