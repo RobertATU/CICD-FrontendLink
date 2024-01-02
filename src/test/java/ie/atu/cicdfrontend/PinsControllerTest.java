29d77d4bac1eee3830791342614dfd9076c3e393
Robert <g00395612@atu.ie>
2024-01-02 12:02:27 +0000

    Added tests

--- src/test/java/ie/atu/cicdfrontend/PinsControllerTest.java
+++ src/test/java/ie/atu/cicdfrontend/PinsControllerTest.java
@@ -1,28 +1,44 @@
 package ie.atu.cicdfrontend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Collections;
+import java.awt.*;
 
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(PinsController.class)
-
+@SpringBootTest
+@AutoConfigureMockMvc
 public class PinsControllerTest {
+
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
     @Autowired
-    private MockMvc mockMvc;
-    @MockBean
-    private PinsService pinsService;
+    public PinsControllerTest(MockMvc mockMvc, ObjectMapper objectMapper) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = objectMapper;
+    }
+
 
     @Test
-    public void allPinsTest() throws Exception{
+    public void testAllPins() throws Exception{
+        Pins pins = new Pins();
+        pins.setSheepId("newSheep");
+        pins.setLongitude(20.321);
+        pins.setLatitude(20.123);
+
+        String requestBody = objectMapper.writeValueAsString(pins);
+
+        mockMvc.perform(post("/pins/createPin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(status().isOk());
 
     } 
 }
